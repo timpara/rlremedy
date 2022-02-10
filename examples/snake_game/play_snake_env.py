20cfbc10c7457721b6d24ef380a172f753eea642
t25541 <t25541@uniper.energy>
2022-02-10 18:17:18 +0100

    Add play snake routine

---
+++ examples/snake_game/play_snake_env.py
@@ -0,0 +1,22 @@
+import gym
+from stable_baselines3 import PPO
+import os
+from snake_env import snakeEnv
+
+
+env = snakeEnv() # continuous: LunarLanderContinuous-v2
+env.reset()
+
+model_path = os.path.join("models","1644505910","80000.zip")
+
+model = PPO.load(model_path, env=env)
+
+episodes = 50
+
+for ep in range(episodes):
+    obs = env.reset()
+    done = False
+    while not done:
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+        print(rewards)
