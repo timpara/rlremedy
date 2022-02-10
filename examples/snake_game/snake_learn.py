7dfb1f47ec47ce6f9e600c4f7f430beb78076675
t25541 <t25541@uniper.energy>
2022-02-10 16:13:21 +0100

    Add snake learn routine

---
+++ examples/snake_game/snake_learn.py
@@ -0,0 +1,27 @@
+from stable_baselines3 import PPO
+import os
+from snake_env import snakeEnv
+import time
+
+
+
+models_dir = os.path.join("models",str(int(time.time())))
+logdir = os.path.join("logs",str(int(time.time())))
+
+if not os.path.exists(models_dir):
+	os.makedirs(models_dir)
+
+if not os.path.exists(logdir):
+	os.makedirs(logdir)
+
+env = snakeEnv()
+env.reset()
+
+model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
+
+TIMESTEPS = 10000
+iters = 0
+while True:
+	iters += 1
+	model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"PPO")
+	model.save(os.path.join(models_dir,str(TIMESTEPS*iters)))
