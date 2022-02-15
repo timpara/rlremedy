95783c2ed61b47e8d00fd8033545731b3d845a01
t25541 <t25541@uniper.energy>
2022-02-15 21:36:47 +0100

    Refactor snake env checks into integration test

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -1,5 +1,7 @@
 from stable_baselines3.common.env_checker import check_env
 from custom_envs.time_series import time_series_env
+from custom_envs.games import snake_env
+
 import pytest
 
 #%%
@@ -10,5 +12,15 @@ def test_time_series_env():
     :return:
     '''
     env = time_series_env()
+    # It will check your custom environment and output additional warnings if needed
+    check_env(env)
+
+@pytest.mark.integration
+def test_time_series_env():
+    '''
+    Instantiates the snake env and feeds it into the env_check. Output is generated if sanity checks fail.
+    :return:
+    '''
+    env = snake_env()
     # It will check your custom environment and output additional warnings if needed
     check_env(env)
