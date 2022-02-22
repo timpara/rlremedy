404949483790e0dbfade55b0ab972ca6b394d4ed
t25541 <t25541@uniper.energy>
2022-02-22 17:58:03 +0100

    Add rendering

--- custom_envs/time_series.py
+++ custom_envs/time_series.py
@@ -2,6 +2,36 @@ import gym
 from gym import spaces
 import numpy as np
 from collections import deque
+import matplotlib.pyplot as plt
+
+plt.ion()
+class DynamicUpdate():
+    #Suppose we know the x range
+    min_x = 0
+    max_x = 1
+
+    def on_launch(self):
+        #Set up plot
+        self.figure, self.ax = plt.subplots()
+        self.lines, = self.ax.plot([],[], 'o')
+        #Autoscale on unknown axis and known lims on the other
+        self.ax.set_autoscaley_on(True)
+        self.ax.set_xlim(self.min_x, self.max_x)
+        #Other stuff
+        self.ax.grid()
+
+
+    def on_running(self, xdata, ydata):
+        #Update data (with the new _and_ the old points)
+        self.lines.set_xdata(xdata)
+        self.lines.set_ydata(ydata)
+        #Need both of these in order to rescale
+        self.ax.relim()
+        self.ax.autoscale_view()
+        #We need to draw *and* flush
+        self.figure.canvas.draw()
+        self.figure.canvas.flush_events()
+
 
 
 
@@ -50,14 +80,13 @@ class time_series_env(gym.Env):
 
         self.total_reward += self.reward
 
-
-
+        self.all_previous_actions.append(action)
         # create observation:
         info = {"Total_reward": self.total_reward, "tick_count": self.tick_count}
         observation = [self.data_at_step[0], self.data_at_step[1]] + list(self.prev_actions)
-        observation = np.reshape(np.array(observation),[-1,1])
+        self.observation = np.reshape(np.array(observation),[-1,1])
         self.tick_count +=1
-        return observation, self.total_reward, self.done, info
+        return self.observation, self.total_reward, self.done, info
 
     def reset(self):
         # Initial action
@@ -67,11 +96,15 @@ class time_series_env(gym.Env):
         self.data_at_step = deque(maxlen=2)
         self.tick_count = 0
         self.done = False
-
+        self.all_previous_actions = []
         #create toy sin data
-        self.my_data = np.reshape(np.sin(np.random.rand(1000) * 2),[-1,1])
+        # 10k linearly spaced numbers
 
-        # however long we aspire the snake to be
+        self.ticks = np.arange(0, 1000)
+        self.my_data = np.reshape(np.sin(np.linspace(-np.pi*60, 60*np.pi, 1000)*100)+1,[-1,1])
+
+
+        self._first_rendering=True
         for _ in range(2):
             self.prev_actions.append(-1)  # to create history
             self.data_at_step.append(0)
@@ -80,3 +113,36 @@ class time_series_env(gym.Env):
         observation = np.reshape(np.array(observation),[-1,1])
 
         return observation
+
+    def render(self, mode="human"):
+        if self.tick_count < 100:
+            return
+
+
+        def _plot_position():
+            color = None
+
+            if self.all_previous_actions[-1]==1:
+                color = 'green'
+            elif self.all_previous_actions[-1]==2:
+                color = 'red'
+
+            if color:
+                plt.scatter(self.tick_count,self.my_data[self.tick_count-1], color=color)
+
+        if self._first_rendering:
+            self._first_rendering = False
+            plt.cla()
+            plt.plot(self.my_data)
+            _plot_position()
+        plt.plot(self.my_data)
+        _plot_position()
+
+        plt.suptitle(
+            "Total Reward: %.6f" % self.total_reward)
+
+
+        plt.pause(0.01)
+
+    def pause_rendering(self):
+        plt.show()
---
+++ examples/time_series/train.py
@@ -0,0 +1,27 @@
+from stable_baselines3 import PPO
+import os
+from custom_envs.time_series import time_series_env
+import time
+
+
+
+models_dir = os.path.join("models",str(int(time.time())))
+logdir = os.path.join("logs",str(int(time.time())))
+
+if not os.path.exists(models_dir):
+	os.makedirs(models_dir)
+
+if not os.path.exists(logdir):
+	os.makedirs(logdir)
+
+env = time_series_env()
+env.reset()
+
+model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
+
+TIMESTEPS = 10000
+iters = 0
+while True:
+	iters += 1
+	model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"PPO")
+	model.save(os.path.join(models_dir,str(TIMESTEPS*iters)))
