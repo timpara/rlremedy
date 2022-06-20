6cecb6b9126e6e1d8e073cd137167dbbde04980c
t25541 <t25541@uniper.energy>
2022-06-20 22:38:55 +0200

    Add custom policy

---
+++ rlremedy/policies/actor_critic.py
@@ -0,0 +1,39 @@
+from typing import Callable, Dict, List, Optional, Type, Union
+
+import gym
+from torch import nn
+
+from stable_baselines3.common.policies import ActorCriticPolicy
+
+from rlremedy.models.neural_nets.policy_value_extractors import CustomNetwork
+
+
+class CustomActorCriticPolicy(ActorCriticPolicy):
+    def __init__(
+        self,
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        lr_schedule: Callable[[float], float],
+        net_arch: Optional[List[Union[int, Dict[str, List[int]]]]] = None,
+        activation_fn: Type[nn.Module] = nn.ReLU,
+        *args,
+        **kwargs,
+    ):
+
+        super(CustomActorCriticPolicy, self).__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            net_arch,
+            activation_fn,
+            # Pass remaining arguments to base class
+            *args,
+            **kwargs,
+        )
+        # Disable orthogonal initialization
+        self.ortho_init = False
+
+    def _build_mlp_extractor(self) -> None:
+        self.mlp_extractor = CustomNetwork(self.features_dim)
+
+
